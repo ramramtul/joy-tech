fe647ed4c25658e709e0f18c60ab779d9f99622d
rama rahmatullah <rama@tjetak.com>
2024-01-31 12:54:19 +0700

    refactor: better error handling

--- pkg/book/service.go
+++ pkg/book/service.go
@@ -2,37 +2,35 @@ package book
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"joy-tech/models"
-	"log"
 	"net/http"
 )
 
 type BookService struct{}
 
-func GetBookList(url string, limit int, offset int) (models.BookPagination, error) {
+func GetBookList(url string, limit int, offset int) (*models.BookPagination, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Print(err.Error())
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	subjects := models.Subject{}
 
 	jsonErr := json.Unmarshal(body, &subjects)
 	if jsonErr != nil {
-		log.Fatal(jsonErr)
+		return nil, err
 	}
 
 	models.BookList = subjects.Works
 
-	pagination := models.BookPagination{
+	pagination := &models.BookPagination{
 		Data:   subjects.Works,
 		Limit:  limit,
 		Offset: offset,
